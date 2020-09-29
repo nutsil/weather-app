d811b6d08d31da713b24211b178b7ee1c53a29fd
nutsil <silpagui.sa@gmail.com>
2020-09-29 18:47:49 -0300

    Add component and styles to WeatherItem

--- src/components/WeatherItem/WeatherItem.component.js
+++ src/components/WeatherItem/WeatherItem.component.js
@@ -1,6 +1,16 @@
 import React from "react";
 import "./WeatherItem.styles.scss";
+import sun from "../../assets/images/sun.svg";
 
-function WeatherItem() {}
+function WeatherItem() {
+  return (
+    <div className="weather-item">
+      <p className="date">Saturday</p>
+      <img src={sun} alt="sun" />
+      <p className="temperature">23º</p>
+      <p className="description-sky">Clear Sky</p>
+    </div>
+  );
+}
 
 export default WeatherItem;
