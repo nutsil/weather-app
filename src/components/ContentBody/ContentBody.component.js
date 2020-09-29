7ad2ac5b7dc0cc26282ad52ebe669581f51a9486
nutsil <silpagui.sa@gmail.com>
2020-09-29 18:17:14 -0300

    Add ExtendedWeather component to ContentBody

--- src/components/ContentBody/ContentBody.component.js
+++ src/components/ContentBody/ContentBody.component.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./ContentBody.styles.scss";
 import SearchBar from "../SearchBar/SearchBar.component";
 import DayWeather from "../DayWeather/DayWeather.component";
+import ExtendedWeather from "../ExtendedWeather/ExtendedWeather.component";
 
 function ContentBody() {
   return (
     <div className="content-wrapper">
       <SearchBar />
       <DayWeather />
+      <ExtendedWeather />
     </div>
   );
 }
