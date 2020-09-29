46da71ae0a8e4b5539e7744be883e92487498848
nutsil <silpagui.sa@gmail.com>
2020-09-29 17:51:32 -0300

    Add DayWeather component to ContentBody

--- src/components/ContentBody/ContentBody.component.js
+++ src/components/ContentBody/ContentBody.component.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./ContentBody.styles.scss";
 import SearchBar from "../SearchBar/SearchBar.component";
+import DayWeather from "../DayWeather/DayWeather.component";
 
 function ContentBody() {
   return (
     <div className="content-wrapper">
       <SearchBar />
+      <DayWeather />
     </div>
   );
 }
