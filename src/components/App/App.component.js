de620e61de88c70fd6050c45938c96ff96514faf
nutsil <silpagui.sa@gmail.com>
2020-09-28 21:20:08 -0300

    Creater Header

--- src/components/App/App.component.js
+++ src/components/App/App.component.js
@@ -1,8 +1,13 @@
 import React from "react";
 import "./App.styles.scss";
+import Header from "../Header/Header.component";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.component.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.styles.scss";
+
+function Header() {
+  return (
+    <header className="header">
+      <h1>WeatherApp</h1>
+      <div></div>
+    </header>
+  );
+}
+
+export default Header;
