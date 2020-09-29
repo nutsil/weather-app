8f3e099fef32d17516363f26477a0b8c2c9ecea0
nutsil <silpagui.sa@gmail.com>
2020-09-29 13:30:33 -0300

    Create SearchBar component and styles

--- src/components/SearchBar/SearchBar.component.js
+++ src/components/SearchBar/SearchBar.component.js
@@ -1,8 +1,16 @@
 import React from "react";
 import "./SearchBar.styles.scss";
+import search from "../../assets/images/search.svg";
 
 function SearchBar() {
-  return;
+  return (
+    <form className="search-bar">
+      <input type="search" id="search" placeholder="Search for a city..." />
+      <button className="search-button">
+        <img src={search} alt="search" />
+      </button>
+    </form>
+  );
 }
 
 export default SearchBar;
