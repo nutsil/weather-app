2224f40be4d3f50446284ccb759c5bb0f1ac9a0b
nutsil <silpagui.sa@gmail.com>
2020-09-28 21:43:14 -0300

    Add styles to Header

--- src/components/Header/Header.component.js
+++ src/components/Header/Header.component.js
@@ -1,11 +1,20 @@
 import React from "react";
 import "./Header.styles.scss";
+import house from "../../assets/images/house.svg";
+import settings from "../../assets/images/settings.svg";
 
 function Header() {
   return (
     <header className="header">
       <h1>WeatherApp</h1>
-      <div></div>
+      <div>
+        <button className="header-button">
+          <img src={house} alt="home" />
+        </button>
+        <button className="header-button">
+          <img src={settings} alt="settings" />
+        </button>
+      </div>
     </header>
   );
 }
