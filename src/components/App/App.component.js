06dc4df58100def2c85854c3aaf4605e78b5fd70
nutsil <silpagui.sa@gmail.com>
2020-09-28 21:52:04 -0300

    Create component ContentBody

--- src/components/App/App.component.js
+++ src/components/App/App.component.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.styles.scss";
 import Header from "../Header/Header.component";
+import ContentBody from "../ContentBody/ContentBody.component";
 
 function App() {
   return (
     <div>
       <Header />
+      <ContentBody />
     </div>
   );
 }
---
+++ src/components/ContentBody/ContentBody.component.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ContentBody.styles.scss";
+
+function ContentBody() {
+  return <div className="content-wrapper"></div>;
+}
+
+export default ContentBody;
